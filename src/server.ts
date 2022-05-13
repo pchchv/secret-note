4cf43fb1d2ea37c8e600d4a88d338c5ec9562b3a
pchchv <ipchchv@gmail.com>
2022-05-13 10:52:12 +0300

    Fix application run

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import 'module-alias/register'
 import 'source-map-support/register'
-import { encrypt, decrypt } from '@/processors/crypto'
-import { getHashedKey } from '@/processors/key'
+import runApp from '@/helpers/runApp'
+
+void (async () => {
+    await runApp()
+})
