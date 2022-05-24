e23c7ff1eceacdb9c130908f0f447230cd76da71
pchchv <ipchchv@gmail.com>
2022-05-24 23:24:22 +0300

    Add DELETE method

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,9 +1,20 @@
+import {
+    Get,
+    Body,
+    Post,
+    Flow,
+    State,
+    Delete,
+    Controller,
+    CurrentUser,
+} from "amala";
 import auth from "@/middleware/auth";
 import { User } from "@/models/User";
 import { encrypt } from "@/processors/crypto";
-import { MessageModel } from "@/models/Message";
 import MessageBody from "@/validators/MessageBody";
-import { Body, Controller, CurrentUser, Flow, Get, Post } from "amala";
+import checkAuthor from "@/middleware/checkAuthor";
+import { DocumentType } from "@typegoose/typegoose";
+import { Message, MessageModel } from "@/models/Message";
 
 @Controller('/message')
 @Flow(auth)
@@ -13,13 +24,13 @@ export default class MessageController {
         @Body({ required: true }) { text }: MessageBody,
         @CurrentUser() author: User
     ) {
-        const separator = '.000.'
+        const separator = '.0.'
         let [key, encryptedText, hashedKey] = encrypt(text)
         MessageModel.create({ author, encryptedText, hashedKey })
         let message = this.getLastMessage(author)
         let id = message._conditions.author._id.valueOf()
-        key = key + separator + id
-        return { key }
+        let userKey = key + separator + id
+        return { userKey }
     }
     
     @Get('/')
@@ -32,5 +43,11 @@ export default class MessageController {
         @CurrentUser() author: User
     ) {
         return MessageModel.find({ author }).sort({ _id: -1}).limit(1)
+    }
+
+    @Delete('/:id/:flag')
+    @Flow(checkAuthor)
+    deleteMessage(@State('message') message: DocumentType<Message>) {
+        return message.deleteOne()
     }
 }
---
+++ src/middleware/checkAuthor.ts
@@ -0,0 +1,19 @@
+import { Context, Next } from 'koa'
+import { MessageModel } from '@/models/Message'
+import { notFound } from '@hapi/boom'
+
+export default async function checkAuthor(ctx: Context, next: Next) {
+    try {
+        const message = await MessageModel.findById(ctx.params.id)
+        if (ctx.params.flag === "true") {
+            const user = ctx.state.user
+            if (message?.author?.toString() != user.id) {
+                throw 'Error'
+            }
+        }
+        ctx.state.message = message
+        return next()
+    } catch {
+        return ctx.throw(notFound())
+    }
+}
