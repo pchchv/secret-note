c56a2b4210c8915c730df18d8fa496c7600fda26
pchchv <ipchchv@gmail.com>
2022-05-24 17:29:07 +0300

    Change the return value of the POST request. Previously: key, now: key + message id.

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -13,10 +13,15 @@ export default class MessageController {
         @Body({ required: true }) { text }: MessageBody,
         @CurrentUser() author: User
     ) {
+        const separator = '.000.'
         let [key, encryptedText, hashedKey] = encrypt(text)
         MessageModel.create({ author, encryptedText, hashedKey })
+        let message = this.getLastMessage(author)
+        let id = message._conditions.author._id.valueOf()
+        key = key + separator + id
         return { key }
     }
+    
     @Get('/')
     getMessage(@CurrentUser() author: User) {
         return MessageModel.find({ author })
