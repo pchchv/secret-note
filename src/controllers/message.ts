c22d6f530aa627125cae9f8f8fcebecfb70a976b
pchchv <ipchchv@gmail.com>
2022-05-28 17:23:41 +0300

    Change the DELETE method. You need the return value as text

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -10,7 +10,7 @@ import {
 } from "amala";
 import auth from "@/middleware/auth";
 import { User } from "@/models/User";
-import { encrypt } from "@/processors/crypto";
+import { decrypt, encrypt } from "@/processors/crypto";
 import MessageBody from "@/validators/MessageBody";
 import checkAuthor from "@/middleware/checkAuthor";
 import { DocumentType } from "@typegoose/typegoose";
@@ -45,9 +45,16 @@ export default class MessageController {
         return MessageModel.find({ author }).sort({ _id: -1}).limit(1)
     }
 
-    @Delete('/:id/:flag')
+    @Delete('/:userKey/:flag')
     @Flow(checkAuthor)
-    deleteMessage(@State('message') message: DocumentType<Message>) {
-        return message.deleteOne()
-    }
+    deleteMessage(
+        @State('key') key: string,
+        @State('message') message: DocumentType<Message>
+        ) {
+            let result = decrypt(message.encryptedText, key, message.hashedKey)
+            if (result != 'Error! Invalid key.') {
+                message.deleteOne()
+            }
+            return { result }
+        }
 }
--- src/middleware/checkAuthor.ts
+++ src/middleware/checkAuthor.ts
@@ -3,14 +3,16 @@ import { MessageModel } from '@/models/Message'
 import { notFound } from '@hapi/boom'
 
 export default async function checkAuthor(ctx: Context, next: Next) {
+    const [key, id] = ctx.params.userKey.split('.0.')
     try {
-        const message = await MessageModel.findById(ctx.params.id)
+        const message = await MessageModel.findById(id)
         if (ctx.params.flag === "true") {
             const user = ctx.state.user
             if (message?.author?.toString() != user.id) {
                 throw 'Error'
             }
         }
+        ctx.state.key = key
         ctx.state.message = message
         return next()
     } catch {
