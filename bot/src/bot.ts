7ab62cfd56d7268ab20b95d70f7263807693c73b
pchchv <ipchchv@gmail.com>
2022-05-29 05:03:34 +0300

    Add help command on bot

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -8,7 +8,19 @@ const bot: Telegraf<Context<Update>> = new Telegraf(env.TOKEN)
 
 bot.start((ctx) => {
     ctx.reply('Привет ' + ctx.from.first_name + '!')
+    ctx.reply('Этот бот создаёт защищённые заметки.\n'
+    + 'Чтобы узнать больше, отправь команду /help')
 })
+
+bot.help((ctx) => {
+    ctx.reply('/post '
+    + 'для отправки сообщения которое сможешь прочитать только ты\n\n'
+    + '/allpost '
+    + 'для отправки сообщения которое сможет прочесть кто-то другой\n'
+    + '(по паролю)\n\n'
+    + '/get для получения сообщения')
+})
+
 
 bot.launch()
 console.log('Bot launched')
