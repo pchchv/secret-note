42252e865fd28fe3510f2e4c968c67714a8f72a2
pchchv <ipchchv@gmail.com>
2022-06-01 12:54:39 +0300

    Fix for deploy

--- bot/src/helpers/get.ts
+++ bot/src/helpers/get.ts
@@ -5,7 +5,6 @@ import { Context } from "telegraf";
 export function get(key: string, user: number, ctx: Context) {
     const url = 'http://' + env.URL + '/message/' + key + '/' + user
     axios.delete(url).then(resp => {
-        console.log(resp.data.result)
         ctx.reply(resp.data.result)
     })
 }
